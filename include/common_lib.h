888f9e07b879f9ea17b4b84526c5fc3e41611a9f
Jeff Hykin <jeff.hykin@gmail.com>
2026-06-03 15:21:19 -0700

    pointlio: ROS-strip common_lib.h (custom_messages bridge, non-ROS time helpers)

--- include/common_lib.h
+++ include/common_lib.h
@@ -5,180 +5,105 @@
 #include <Eigen/Eigen>
 #include <pcl/point_types.h>
 #include <pcl/point_cloud.h>
-#include "msgs.h"
-
+#include "msgs.h"            // custom_messages (non-ROS): Imu, Odometry, Time, ...
+#include <boost/shared_ptr.hpp>
+#include <deque>
 using namespace std;
 using namespace Eigen;
 
-#define USE_IKFOM
-
 #define PI_M (3.14159265358)
-#define G_m_s2 (9.81)         // Gravaty const in GuangDong/China
+#define G_m_s2 (9.81)         // Gravity const in GuangDong/China
 #define DIM_STATE (18)        // Dimension of states (Let Dim(SO(3)) = 3)
 #define DIM_PROC_N (12)       // Dimension of process noise (Let Dim(SO(3)) = 3)
 #define CUBE_LEN  (6.0)
 #define LIDAR_SP_LEN    (2)
-#define INIT_COV   (1)
+#define INIT_COV   (0.0001)
 #define NUM_MATCH_POINTS    (5)
 #define MAX_MEAS_DIM        (10000)
 
 #define VEC_FROM_ARRAY(v)        v[0],v[1],v[2]
+#define VEC_FROM_ARRAY_SIX(v)    v[0],v[1],v[2],v[3],v[4],v[5]
 #define MAT_FROM_ARRAY(v)        v[0],v[1],v[2],v[3],v[4],v[5],v[6],v[7],v[8]
 #define CONSTRAIN(v,min,max)     ((v>min)?((v<max)?v:max):min)
 #define ARRAY_FROM_EIGEN(mat)    mat.data(), mat.data() + mat.rows() * mat.cols()
 #define STD_VEC_FROM_EIGEN(mat)  vector<decltype(mat)::Scalar> (mat.data(), mat.data() + mat.rows() * mat.cols())
-#define ROOT_DIR                 "../"
 #define DEBUG_FILE_DIR(name)     (string(string(ROOT_DIR) + "Log/"+ name))
 
-typedef boost::shared_ptr< custom_messages::Imu const> ImuConstPtr;
-
-typedef custom_messages::Pose6D Pose6D;
 typedef pcl::PointXYZINormal PointType;
-typedef pcl::PointCloud<PointType> PointCloudXYZI;
+typedef pcl::PointXYZRGB     PointTypeRGB;
+typedef pcl::PointCloud<PointType>    PointCloudXYZI;
+typedef pcl::PointCloud<PointTypeRGB> PointCloudXYZRGB;
 typedef vector<PointType, Eigen::aligned_allocator<PointType>>  PointVector;
 typedef Vector3d V3D;
 typedef Matrix3d M3D;
 typedef Vector3f V3F;
 typedef Matrix3f M3F;
 
+// Non-ROS IMU message pointer (mirrors the FAST-LIO2 non-ROS module bridge).
+typedef boost::shared_ptr<custom_messages::Imu const> ImuConstPtr;
+
 #define MD(a,b)  Matrix<double, (a), (b)>
 #define VD(a)    Matrix<double, (a), 1>
 #define MF(a,b)  Matrix<float, (a), (b)>
 #define VF(a)    Matrix<float, (a), 1>
 
-M3D Eye3d(M3D::Identity());
-M3F Eye3f(M3F::Identity());
-V3D Zero3d(0, 0, 0);
-V3F Zero3f(0, 0, 0);
+const M3D Eye3d(M3D::Identity());
+const M3F Eye3f(M3F::Identity());
+const V3D Zero3d(0, 0, 0);
+const V3F Zero3f(0, 0, 0);
 
 struct MeasureGroup     // Lidar data and imu dates for the curent process
 {
     MeasureGroup()
     {
         lidar_beg_time = 0.0;
+        lidar_last_time = 0.0;
         this->lidar.reset(new PointCloudXYZI());
     };
     double lidar_beg_time;
-    double lidar_end_time;
+    double lidar_last_time;
     PointCloudXYZI::Ptr lidar;
-    deque<ImuConstPtr> imu;
+    deque<ImuConstPtr> imu{};
 };
 
-struct StatesGroup
-{
-    StatesGroup() {
-		this->rot_end = M3D::Identity();
-		this->pos_end = Zero3d;
-        this->vel_end = Zero3d;
-        this->bias_g  = Zero3d;
-        this->bias_a  = Zero3d;
-        this->gravity = Zero3d;
-        this->cov     = MD(DIM_STATE,DIM_STATE)::Identity() * INIT_COV;
-        this->cov.block<9,9>(9,9) = MD(9,9)::Identity() * 0.00001;
-	};
+template <typename T>
+T calc_dist(PointType p1, PointType p2){
+    T d = (p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y) + (p1.z - p2.z) * (p1.z - p2.z);
+    return d;
+}
 
-    StatesGroup(const StatesGroup& b) {
-		this->rot_end = b.rot_end;
-		this->pos_end = b.pos_end;
-        this->vel_end = b.vel_end;
-        this->bias_g  = b.bias_g;
-        this->bias_a  = b.bias_a;
-        this->gravity = b.gravity;
-        this->cov     = b.cov;
-	};
-
-    StatesGroup& operator=(const StatesGroup& b)
-	{
-        this->rot_end = b.rot_end;
-		this->pos_end = b.pos_end;
-        this->vel_end = b.vel_end;
-        this->bias_g  = b.bias_g;
-        this->bias_a  = b.bias_a;
-        this->gravity = b.gravity;
-        this->cov     = b.cov;
-        return *this;
-	};
-
-    StatesGroup operator+(const Matrix<double, DIM_STATE, 1> &state_add)
-	{
-        StatesGroup a;
-		a.rot_end = this->rot_end * Exp(state_add(0,0), state_add(1,0), state_add(2,0));
-		a.pos_end = this->pos_end + state_add.block<3,1>(3,0);
-        a.vel_end = this->vel_end + state_add.block<3,1>(6,0);
-        a.bias_g  = this->bias_g  + state_add.block<3,1>(9,0);
-        a.bias_a  = this->bias_a  + state_add.block<3,1>(12,0);
-        a.gravity = this->gravity + state_add.block<3,1>(15,0);
-        a.cov     = this->cov;
-		return a;
-	};
-
-    StatesGroup& operator+=(const Matrix<double, DIM_STATE, 1> &state_add)
-	{
-        this->rot_end = this->rot_end * Exp(state_add(0,0), state_add(1,0), state_add(2,0));
-		this->pos_end += state_add.block<3,1>(3,0);
-        this->vel_end += state_add.block<3,1>(6,0);
-        this->bias_g  += state_add.block<3,1>(9,0);
-        this->bias_a  += state_add.block<3,1>(12,0);
-        this->gravity += state_add.block<3,1>(15,0);
-		return *this;
-	};
-
-    Matrix<double, DIM_STATE, 1> operator-(const StatesGroup& b)
-	{
-        Matrix<double, DIM_STATE, 1> a;
-        M3D rotd(b.rot_end.transpose() * this->rot_end);
-        a.block<3,1>(0,0)  = Log(rotd);
-        a.block<3,1>(3,0)  = this->pos_end - b.pos_end;
-        a.block<3,1>(6,0)  = this->vel_end - b.vel_end;
-        a.block<3,1>(9,0)  = this->bias_g  - b.bias_g;
-        a.block<3,1>(12,0) = this->bias_a  - b.bias_a;
-        a.block<3,1>(15,0) = this->gravity - b.gravity;
-		return a;
-	};
-
-    void resetpose()
-    {
-        this->rot_end = M3D::Identity();
-		this->pos_end = Zero3d;
-        this->vel_end = Zero3d;
-    }
-
-	M3D rot_end;      // the estimated attitude (rotation matrix) at the end lidar point
-    V3D pos_end;      // the estimated position at the end lidar point (world frame)
-    V3D vel_end;      // the estimated velocity at the end lidar point (world frame)
-    V3D bias_g;       // gyroscope bias
-    V3D bias_a;       // accelerator bias
-    V3D gravity;      // the estimated gravity acceleration
-    Matrix<double, DIM_STATE, DIM_STATE>  cov;     // states covariance
-};
-
-template<typename T>
-T rad2deg(T radians)
-{
-  return radians * 180.0 / PI_M;
+template <typename T>
+T calc_dist(Eigen::Vector3d p1, PointType p2){
+    T d = (p1(0) - p2.x) * (p1(0) - p2.x) + (p1(1) - p2.y) * (p1(1) - p2.y) + (p1(2) - p2.z) * (p1(2) - p2.z);
+    return d;
 }
 
 template<typename T>
-T deg2rad(T degrees)
+std::vector<int> time_compressing(const PointCloudXYZI::Ptr &point_cloud)
 {
-  return degrees * PI_M / 180.0;
-}
-
-template<typename T>
-auto set_pose6d(const double t, const Matrix<T, 3, 1> &a, const Matrix<T, 3, 1> &g, \
-                const Matrix<T, 3, 1> &v, const Matrix<T, 3, 1> &p, const Matrix<T, 3, 3> &R)
-{
-    Pose6D rot_kp;
-    rot_kp.offset_time = t;
-    for (int i = 0; i < 3; i++)
+  int points_size = point_cloud->points.size();
+  int j = 0;
+  std::vector<int> time_seq;
+  // time_seq.clear();
+  time_seq.reserve(points_size);
+  for(int i = 0; i < points_size - 1; i++)
+  {
+    j++;
+    if (point_cloud->points[i+1].curvature > point_cloud->points[i].curvature)
     {
-        rot_kp.acc[i] = a(i);
-        rot_kp.gyr[i] = g(i);
-        rot_kp.vel[i] = v(i);
-        rot_kp.pos[i] = p(i);
-        for (int j = 0; j < 3; j++)  rot_kp.rot[i*3+j] = R(i,j);
+      time_seq.emplace_back(j);
+      j = 0;
     }
-    return move(rot_kp);
+  }
+  if (j == 0)
+  {
+    time_seq.emplace_back(1);
+  }
+  else
+  {
+    time_seq.emplace_back(j+1);
+  }
+  return time_seq;
 }
 
 /* comment
@@ -203,7 +128,7 @@ bool esti_normvector(Matrix<T, 3, 1> &normvec, const PointVector &point, const T
         A(j,2) = point[j].z;
     }
     normvec = A.colPivHouseholderQr().solve(b);
-    
+
     for (int j = 0; j < point_num; j++)
     {
         if (fabs(normvec(0) * point[j].x + normvec(1) * point[j].y + normvec(2) * point[j].z + 1.0f) > threshold)
@@ -214,11 +139,6 @@ bool esti_normvector(Matrix<T, 3, 1> &normvec, const PointVector &point, const T
 
     normvec.normalize();
     return true;
-}
-
-float calc_dist(PointType p1, PointType p2){
-    float d = (p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y) + (p1.z - p2.z) * (p1.z - p2.z);
-    return d;
 }
 
 template<typename T>
@@ -253,6 +173,19 @@ bool esti_plane(Matrix<T, 4, 1> &pca_result, const PointVector &point, const T &
         }
     }
     return true;
+}
+
+// Non-ROS time helpers: custom_messages::Time carries sec/nsec with toSec()/fromSec().
+inline double get_time_sec(const custom_messages::Time &time)
+{
+  return time.toSec();
+}
+
+inline custom_messages::Time get_ros_time(double timestamp)
+{
+  custom_messages::Time t;
+  t.fromSec(timestamp);
+  return t;
 }
 
 #endif
